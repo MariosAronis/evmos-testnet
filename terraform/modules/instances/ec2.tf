b25c324fa21992e2e0f0286a9fbbac6d65b8dfe7
testuser-at-490752553772 <m_aronis@hotmail.com>
2023-11-26 12:11:45 +0200

    validator cloud-init

--- terraform/modules/instances/ec2.tf
+++ terraform/modules/instances/ec2.tf
@@ -41,6 +41,16 @@ resource "aws_instance" "evmos-validator" {
 #!/bin/bash
 apt-get update
 apt-get install -y net-tools
+apt-get install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin gcc make jq -y
+groupadd docker
+usermod -aG docker $USER
 hostnamectl set-hostname "evmos-validator-${count.index}"
 EOF
 
