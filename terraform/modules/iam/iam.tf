e80f39cf86940bacdd744a89277332afe96f43ea
testuser-at-490752553772 <m_aronis@hotmail.com>
2023-11-28 22:13:08 +0200

    remove tag?

--- terraform/modules/iam/iam.tf
+++ terraform/modules/iam/iam.tf
@@ -48,9 +48,9 @@ resource "aws_iam_openid_connect_provider" "github_actions" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = var.thumbprint_list
-  tags = {
-    Name = "GithubUser"
-  }
+#   tags = {
+#     Name = "GithubUser"
+#   }
 }
 
 output "openid_connect_provider" {
