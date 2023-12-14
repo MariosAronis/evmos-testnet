7b1525fe5eed12c674c8d7092c4742db0d9138b4
testuser-at-490752553772 <m_aronis@hotmail.com>
2023-12-14 19:25:37 +0200

    install unzip

--- terraform/modules/instances/ec2.tf
+++ terraform/modules/instances/ec2.tf
@@ -48,7 +48,7 @@ chmod a+r /etc/apt/keyrings/docker.gpg
 echo   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
 "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin gcc make jq -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin gcc make jq unzip -y
 wget https://go.dev/dl/go1.21.4.linux-amd64.tar.gz
 USER=`getent passwd 1000 | cut -d: -f1`
 tar -C /home/$USER -xzf go1.21.4.linux-amd64.tar.gz
